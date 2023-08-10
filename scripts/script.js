f4c751177ccef2624e4a866d40c3207200c4e5b6
dkavla <dkowalczyk135@gmail.com>
2023-08-10 14:20:43 -0400

    Add resetGame(), getSlotIndex(), and add event listeners to reset button, and slots in board

--- scripts/script.js
+++ scripts/script.js
@@ -9,8 +9,8 @@ let Game = (() => {
             ' ', ' ', ' '
         ],
         fillSlot: function(marker, slot) {
-            if (this.board[slot-1] == ' ') {
-                this.board[slot-1] = marker;
+            if (this.board[slot] == ' ') {
+                this.board[slot] = marker;
                 _openSlots--;
             } else {
                 console.log('Slot is filled already. Pick another')
@@ -18,6 +18,14 @@ let Game = (() => {
         },
         getOpenSlots: function() {
             return _openSlots;
+        },
+        resetGame: function() {
+            this.board = [
+                ' ', ' ', ' ',
+                ' ', ' ', ' ',
+                ' ', ' ', ' '
+            ];
+            _openSlots = 9;
         },
         isEmpty: function() {
             return _openSlots === 9;
@@ -34,46 +42,78 @@ let Game = (() => {
 
                 displayWinner.innerText = `Winner ${this.board[0]}`;
                 statusContainer.appendChild(displayWinner);
+                return true;
 
         } else if (this.board[0] === this.board[3] && this.board[0] === this.board[6]) {
 
                 displayWinner.innerText = `Winner ${this.board[0]}`;
                 statusContainer.appendChild(displayWinner);
+                return true;
 
         } else if (this.board[0] === this.board[4] && this.board[0] === this.board[8]) {
 
                 displayWinner.innerText = `Winner ${this.board[0]}`;
                 statusContainer.appendChild(displayWinner);
+                return true;
 
         } else if (this.board[1] === this.board[4] && this.board[1] === this.board[7]) {
 
             displayWinner.innerText = `Winner ${this.board[1]}`;
             statusContainer.appendChild(displayWinner);
+            return true;
 
         } else if (this.board[2] === this.board[5] && this.board[2] === this.board[8]) {
 
             displayWinner.innerText = `Winner ${this.board[2]}`;
             statusContainer.appendChild(displayWinner);
+            return true;
 
         } else if (this.board[2] === this.board[4] && this.board[2] === this.board[6]) {
 
             displayWinner.innerText = `Winner ${this.board[2]}`;
             statusContainer.appendChild(displayWinner);
+            return true;
 
         } else if (this.board[3] === this.board[4] && this.board[3] === this.board[5]) {
 
             displayWinner.innerText = `Winner ${this.board[3]}`;
             statusContainer.appendChild(displayWinner);
+            return true;
 
         } else if (this.board[6] === this.board[7] && this.board[6] === this.board[8]) {
 
             displayWinner.innerText = `Winner ${this.board[6]}`;
             statusContainer.appendChild(displayWinner);
+            return true;
 
+        } else {
+            return false;
         }
     }
 
-    return { GameBoard, checkWinner };
+    let getSlotIndex = (numWord) => {
+        if (numWord === "one") {
+            return 0;
+        } else if (numWord === "two") {
+            return 1;
+        } else if (numWord === "three") {
+            return 2;
+        } else if (numWord === "four") {
+            return 3;
+        } else if (numWord === "five") {
+            return 4;
+        } else if (numWord === "six") {
+            return 5;
+        } else if (numWord === "seven") {
+            return 6;
+        } else if (numWord === "eight") {
+            return 7;
+        } else {
+            return 8;
+        }
+    }
+
+    return { GameBoard, checkWinner, getSlotIndex };
 })();
 
 const Player = (marker) => {
@@ -84,8 +124,35 @@ const Player = (marker) => {
 }
 
 let currentPlayer = 'X'; // track current player
-
 // Create Player Objects
 let p1 = Player('X');
 let p2 = Player('O');
 
+// resets the board
+const resetBtn = document.querySelector('.reset');
+const posSlot = document.querySelectorAll('.slot');
+
+resetBtn.addEventListener("click", () => {
+    Game.GameBoard.resetGame
+    posSlot.forEach((slot) => {
+        slot.innerText = '';
+        Game.GameBoard.resetGame();
+    })
+})
+
+// fills the slot with the current player's marker when clicked
+// Iterates over all slots to add click event 
+posSlot.forEach((slot) => {
+    slot.addEventListener("click", () => {
+        slot.innerText = currentPlayer;
+
+        // calls fillSlot to fill the position in the Gameboard
+        // objet's array board
+        Game.GameBoard.fillSlot(currentPlayer, Game.getSlotIndex(slot.id));
+        if (currentPlayer === 'X') {
+            currentPlayer = 'O';
+        } else {
+            currentPlayer = 'X';
+        }
+    });
+});
